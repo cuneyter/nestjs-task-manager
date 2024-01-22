2f117f3f9e584fc1cb4d9a7837f82720053d425d
Cuneyt Ergun <cuneytergun@gmail.com>
2024-01-22 13:31:15 +0000

    Add jwt.strategy.ts

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,6 +1,7 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Post, UseGuards } from '@nestjs/common';
 import { AuthCredentialsDto } from './dto/auth-credentials.dto';
 import { AuthService } from './auth.service';
+import { AuthGuard } from '@nestjs/passport';
 
 @Controller('auth')
 export class AuthController {
@@ -17,5 +18,12 @@ export class AuthController {
     @Body() authCredentialsDto: AuthCredentialsDto,
   ): Promise<{ accessToken: string }> {
     return this.authService.signIn(authCredentialsDto);
+  }
+
+  // This is just a test route to check if the JWT authentication is working
+  @Post('/test')
+  @UseGuards(AuthGuard())
+  test(@Body() authCredentialsDto: AuthCredentialsDto) {
+    console.log(authCredentialsDto);
   }
 }
