a67f3b8b1c1f11457e9816910e559b7b7b1032e8
Cuneyt Ergun <cuneytergun@gmail.com>
2024-01-22 15:43:10 +0000

    Add restrict all tasks actions by user

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -18,8 +18,10 @@ export class TasksService {
     return this.tasksRepository.createTask(createTaskDto, user);
   }
 
-  async getTaskById(id: string): Promise<Task> {
-    const task: Task = await this.tasksRepository.findOne({ where: { id } });
+  async getTaskById(id: string, user: User): Promise<Task> {
+    const task: Task = await this.tasksRepository.findOne({
+      where: { id, user },
+    });
 
     if (!task) {
       throw new NotFoundException(`Task with ID "${id}" not found`);
@@ -28,9 +30,9 @@ export class TasksService {
     return task;
   }
 
-  async deleteTaskById(id: string): Promise<void> {
+  async deleteTaskById(id: string, user: User): Promise<void> {
     // Preferred method is `delete` over `remove` because it is more efficient
-    const result = await this.tasksRepository.delete({ id });
+    const result = await this.tasksRepository.delete({ id, user });
 
     if (result.affected === 0) {
       throw new NotFoundException(`Task with ID "${id}" not found`);
@@ -44,8 +46,12 @@ export class TasksService {
     // await this.tasksRepository.remove(task);
   }
 
-  async updateTaskStatus(id: string, status: TaskStatus): Promise<Task> {
-    const task: Task = await this.getTaskById(id);
+  async updateTaskStatus(
+    id: string,
+    status: TaskStatus,
+    user: User,
+  ): Promise<Task> {
+    const task: Task = await this.getTaskById(id, user);
 
     task.status = status;
     return await this.tasksRepository.save(task);
