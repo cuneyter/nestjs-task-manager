098de7c49ee54374dad1538f2ecb898f3e4b8ab0
Cuneyt Ergun <cuneytergun@gmail.com>
2024-01-22 13:46:42 +0000

    Add Guarding the tasks routes

--- src/tasks/tasks.module.ts
+++ src/tasks/tasks.module.ts
@@ -4,9 +4,10 @@ import { TasksService } from './tasks.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { TasksRepository } from './tasks.repository';
 import { Task } from './task.entity';
+import { AuthModule } from '../auth/auth.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Task])],
+  imports: [AuthModule, TypeOrmModule.forFeature([Task])],
   providers: [TasksService, TasksRepository],
   controllers: [TasksController],
 })
